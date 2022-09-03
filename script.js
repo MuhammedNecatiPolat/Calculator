024b167b1f2c7a9fb4c897ccf9376fb6f43bb2fa
Muhammed Necati Polat <mnecatipolat@gmail.com>
2022-09-04 02:37:13 +0300

    add event listeners to buttons and numbers

--- script.js
+++ script.js
@@ -10,4 +10,27 @@ const operate = (a, b, operator) => {
     if (operator = '/') return divide(a, b);
 } 
 
-const displayOnClick = () =>
+let str = '';
+let myList = [];
+const numbers = document.querySelectorAll('.number')
+const displayScreen = document.querySelector('.display');
+
+
+numbers.forEach((number) => number.addEventListener('click', () => {
+    str += number.textContent;
+    myString = document.createElement('div');
+    myString.textContent = str;
+    displayScreen.appendChild(myString);
+
+    }))
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach( (operator) => operator.addEventListener('click', () => {
+    myList.push(str);
+    str = '';
+    myList.push(operator.textContent);
+    if (operator.textContent == '='){
+
+    }
+}))
