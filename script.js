d796ef2e6ff4bdbb4bc5c5f6645f606d094bfa2a
Muhammed Necati Polat <mnecatipolat@gmail.com>
2022-09-04 13:45:03 +0300

    add Calculate and display result functions

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const multiply = (a, b) => {return a * b};
 const divide = (a, b) => {return a / b};
 
 const operate = (a, b, operator) => {
-    if (operator = '+') return add(a, b);
-    if (operator = '-') return subtract(a, b);
-    if (operator = '*') return multiply(a, b);
-    if (operator = '/') return divide(a, b);
+    if (operator == '+') return add(a, b);
+    if (operator =='-') return subtract(a, b);
+    if (operator == '*') return multiply(a, b);
+    if (operator == '/') return divide(a, b);
 } 
 
 let str = '';
@@ -16,6 +16,7 @@ const numbers = document.querySelectorAll('.number')
 const displayScreen = document.querySelector('.display');
 let counter = 0;
 let stringCounter = 0;
+let operatorCounter = 0;
 
 numbers.forEach((number) => number.addEventListener('click', () => {
     str += number.textContent;
@@ -27,30 +28,79 @@ numbers.forEach((number) => number.addEventListener('click', () => {
         counter--;
     }
 
-    const myString = document.createElement('div');
-    myString.textContent = str;
-    myString.classList.add('displayedNumber');
-    displayScreen.appendChild(myString);
+    displayNumber(str);
     counter++;
     }))
 
 const operators = document.querySelectorAll('.operator');
 
 operators.forEach( (operator) => operator.addEventListener('click', () => {
+    
     counter = 0;
     stringCounter++;
     myList.push(str);
     str = '';
 
-    myList.push(operator.textContent);
-    if (operator.textContent == '='){
+    
+    if (operator.textContent == '=' || operator.textContent == 'clear'){
+        removeDisplayedNumbers();
+        removeDisplayedOperators();
+        console.log(myList);
 
+        if(operator.textContent == 'clear'){
+            myList = [];
+            stringCounter = 0;
+        }
+        if(operator.textContent == '='){
+            stringCounter = 0;
+            result = calculateResult(myList);
+            str = result;
+            displayNumber(result);
+        }
+    }
+    else {
+        myList.push(operator.textContent);
+        operatorCounter++;
+        displayOperator(operator.textContent);
     }
 }))
+
+
+let displayOperator = (str) => {
+    const divElement = document.createElement('div');
+    divElement.textContent = str;
+    divElement.classList.add('displayedOperator');
+    displayScreen.appendChild(divElement);
+}
 
 let displayNumber = (str) => {
     const divElement = document.createElement('div');
     divElement.textContent = str;
     divElement.classList.add('displayedNumber');
     displayScreen.appendChild(divElement);
+}
+
+const removeDisplayedNumbers = () => {
+    const displayedNumbers = document.querySelectorAll('.displayedNumber');
+    displayedNumbers.forEach((displayedNumber) => displayedNumber.remove())
+}
+
+const removeDisplayedOperators = () => {
+    const displayedOperator = document.querySelectorAll('.displayedOperator');
+    displayedOperator.forEach((displayedOperator) => displayedOperator.remove())
+}
+
+const calculateResult = (lst) => {
+    while(operatorCounter>0){
+        num1 = +lst.shift();
+        operator = lst.shift();
+        num2 = +lst.shift();
+        if(num1 == 0 && num2 == 0 && operator == '/') return 'no such shit';
+        result = operate(num1, num2, operator);
+        lst.unshift(result);
+        operatorCounter--;
+    }
+
+    result = +lst.shift();
+    return result;
 }
